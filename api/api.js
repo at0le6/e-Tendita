e59b74968657aa149ac31c74c9481ffee5d71ada
angel gamaliel <angel.gamaliel@hotmail.com>
2022-06-25 21:26:10 -0500

    End pont all ok /

--- api/api.js
+++ api/api.js
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(morgan("dev"))
 //rutas van aqui con el prefico de APP
 creatBases()
+app.use("/",(req,res)=>res.statusCode(200))
 app.use(prefix,Routers.productoRouter)
 app.use(prefix,Routers.cuponRouter)
 app.use(prefix,Routers.userRouter)
